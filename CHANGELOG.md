2f4854eecceed361ca7c792b542e3227e67a66d1
Werner Robitza <werner.robitza@gmail.com>
2023-01-10 09:16:14 +0100

    Bump version to 0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # Changelog
 
 
+## v0.3.0 (2023-01-10)
+
+* Various improvements.
+
+  add API, types, github workflows
+
+
 ## v0.2.0 (2022-08-02)
 
 * Fix version variable name.
--- video_fft/__init__.py
+++ video_fft/__init__.py
@@ -1,5 +1,5 @@
 from .video_fft_calculator import VideoFftCalculator, VideoFftData
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["VideoFftCalculator", "VideoFftData"]
